8ba6c9e10efd491c146cdada27510f823044ea77
brunEdo <edobruno.m@gmail.com>
2021-08-31 16:54:09 +0200

    eveno or odd js steps

---
+++ pari e dispari/js/app.js
@@ -0,0 +1,9 @@
+// EVEN OR ODD
+
+// 1. the user take a guess, even or odd
+// 2. generate a random number
+// 3. sum to numbers
+// 4. check if the number is even
+// 5. store and comunicate computer's number
+// 6. check if the user's is right
+// 7. declare the winner
