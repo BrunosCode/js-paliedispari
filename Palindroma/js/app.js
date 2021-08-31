1007af9b820ecad250315db30fa5a18775fb60d2
brunEdo <edobruno.m@gmail.com>
2021-08-31 16:23:43 +0200

    palindrome js steps

---
+++ Palindroma/js/app.js
@@ -0,0 +1,7 @@
+// PALINDROME
+
+// 1. a word to the user
+// 2. function: if the word is palindrome return true
+// 2a. reverse the word
+// 2b. check if the word is palindrome
+// 3. tell the result to the user
