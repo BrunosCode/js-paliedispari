39cc999cc93a83d4a53d8b4e57a35ca7eae2f799
brunEdo <edobruno.m@gmail.com>
2021-08-31 18:21:49 +0200

    palindrome style

--- Palindroma/js/app.js
+++ Palindroma/js/app.js
@@ -2,7 +2,7 @@
 
 // 1. ask a word to the user
 const userWord = () => {
-    return prompt("Give me a word");
+    return document.getElementById("word").value;
 }
 
 // 2. reverse the word
@@ -24,9 +24,13 @@ const isPalindrome = (string) => {
     return false;
 }
 
-// 4. run the script and tell the user the result
-if (isPalindrome(userWord())) {
-    alert("The word is palindrome");
-} else {
-    alert("The word is not palindrome");
-}
+document.getElementById("ask").addEventListener("click", () => {
+    // 4. run the script and tell the user the result
+    if (userWord().length <= 1) {
+        document.getElementById("answer").innerHTML = "The word is not long enough!";
+    } else if (isPalindrome(userWord())) {
+        document.getElementById("answer").innerHTML = "The word is palindrome";
+    } else {
+        document.getElementById("answer").innerHTML = "The word is not palindrome";
+    }
+})
