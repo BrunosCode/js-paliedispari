211db91fff57b127a7f634994f4ae124326de8b9
brunEdo <edobruno.m@gmail.com>
2021-08-31 16:43:38 +0200

    palindrome js script

--- Palindroma/js/app.js
+++ Palindroma/js/app.js
@@ -1,7 +1,32 @@
 // PALINDROME
 
-// 1. a word to the user
-// 2. function: if the word is palindrome return true
-// 2a. reverse the word
-// 2b. check if the word is palindrome
-// 3. tell the result to the user
+// 1. ask a word to the user
+const userWord = () => {
+    return prompt("Give me a word");
+}
+
+// 2. reverse the word
+const reverseThisString = (string) => {
+    let reversedString = "";
+    // return word.split("").reverse().join("");
+    for (let i = string.length - 1; i >= 0; i-- ) {
+        reversedString += string[i];
+    }
+    console.log(reversedString)
+    return reversedString;
+}
+
+// 3. check if the word is palindrome
+const isPalindrome = (string) => {
+    if (string === reverseThisString(string)) {
+        return true;
+    }
+    return false;
+}
+
+// 4. run the script and tell the user the result
+if (isPalindrome(userWord())) {
+    alert("The word is palindrome");
+} else {
+    alert("The word is not palindrome");
+}
